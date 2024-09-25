1dca3e4ddd54b811bd6f2aaaebc8b463906bbba3
Ishu_official <somnathhor12052005@gmail.com>
2024-09-25 22:04:42 +0530

    Update Youtube.py

--- BABYMUSIC/platforms/Youtube.py
+++ BABYMUSIC/platforms/Youtube.py
@@ -25,7 +25,7 @@ async def shell_cmd(cmd):
             return errorz.decode("utf-8")
     return out.decode("utf-8")
 
-
+cookies_file = "BABYMUSIC/assets/radhacookies.txt"
 class YouTubeAPI:
     def __init__(self):
         self.base = "https://www.youtube.com/watch?v="
