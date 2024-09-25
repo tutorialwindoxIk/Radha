332003fe641ae41472610877faf29752e8997fd1
Ishu_official <somnathhor12052005@gmail.com>
2024-09-25 21:01:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BABY-MUSIC/BABYXMUSIC",
+    "https://github.com/tutorialwindoxIk/Radha",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
